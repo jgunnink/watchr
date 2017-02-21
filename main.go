e58c3a3f6fc41a4b197c0c88afe1bbf6bda058da
JK Gunnink <jgunnink@gmail.com>
2017-02-21 16:43:46 +0800

    Changed router, created storm orm based on bolt DB

--- main.go
+++ main.go
@@ -2,30 +2,60 @@ package main
 
 import (
 	"html/template"
+	"log"
 	"net/http"
+
+	"github.com/asdine/storm"
+	"github.com/julienschmidt/httprouter"
 )
 
 func main() {
-	http.HandleFunc("/register", Register)
-	http.Handle("/", http.FileServer(http.Dir("public")))
-	http.ListenAndServe(":8000", nil)
+	s := &server{}
+
+	rtr := httprouter.New()
+	rtr.HandlerFunc("POST", "/register", s.Register)
+	rtr.ServeFiles("/public/*filepath", http.Dir("public"))
+	db, err := storm.Open("watchr.db")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	s.db = db
+	s.router = rtr
+
+	// http.Handle("/app", http.FileServer(http.Dir("public")))
+	http.ListenAndServe(":8000", s.router)
 }
 
 // Register takes the HTTP request and attempts to create a user
-func Register(w http.ResponseWriter, r *http.Request) {
+func (s *server) Register(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
-	name := r.FormValue("user")
+	name := r.FormValue("name")
 	email := r.FormValue("email")
 	user := &User{Name: name, Email: email}
+
+	err := s.db.Save(user)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	log.Println("User registration successful")
 	t, err := template.ParseFiles("public/register.html")
 	if err != nil {
 		panic(err)
 	}
+	log.Println(user)
 	t.Execute(w, user)
+}
+
+type server struct {
+	router *httprouter.Router
+	db     *storm.DB
 }
 
 // User is an object containing an email and a name.
 type User struct {
+	ID    int `storm:"id,increment"`
 	Name  string
 	Email string
 }
